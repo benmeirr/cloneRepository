3ca205d7b511f97f146a474a837b62bce2029f39
benmeirr <benmeir615@gmail.com>
2024-03-03 21:54:56 +0200

    fix duplicate code

--- exmaple/newFile.py
+++ exmaple/newFile.py
@@ -2,15 +2,8 @@
 def print_number():
     for i in range(10):
         print(f'{i}')
-    for i in range(10):
-        print(f'{i}')
-    for i in range(10):
-        print(f'{i}')
-    for i in range(10):
-        print(f'{i}')
+
 
 
 if __name__ == '__main__':
-    print_number()
-    print_number()
     print_number()
